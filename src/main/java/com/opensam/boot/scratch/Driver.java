f27050d75f5ec4bf1371e03df18ef1d0c6a0a85a
opensam <opensam4@gmail.com>
2017-05-25 11:04:12 -0400

    Renamed the base package for component scan

--- src/main/java/com/opensam/boot/scratch/Driver.java
+++ src/main/java/com/opensam/boot/scratch/Driver.java
@@ -1,4 +1,4 @@
-package com.opensam.boot.scratch.driver;
+package com.opensam.boot.scratch;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
