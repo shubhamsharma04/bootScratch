ba8b81ac2d2aa962af49126215a800889bef29ca
opensam <opensam4@gmail.com>
2017-05-26 10:35:43 -0400

    Added a controller for testing

--- src/main/java/com/opensam/boot/scratch/Driver.java
+++ src/main/java/com/opensam/boot/scratch/Driver.java
@@ -1,21 +1,11 @@
 package com.opensam.boot.scratch;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@Controller
-@EnableAutoConfiguration
+@SpringBootApplication
 public class Driver {
 	
-	@RequestMapping("/")
-	@ResponseBody
-	String home() {
-		return "Hello World!";
-	}
-
 	public static void main(String[] args) {
 		SpringApplication.run(Driver.class, args);
 	}
---
+++ src/main/java/com/opensam/boot/scratch/controllers/DefaultController.java
@@ -0,0 +1,25 @@
+package com.opensam.boot.scratch.controllers;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class DefaultController {
+	
+	final static Logger logger = Logger.getLogger(DefaultController.class);
+	
+	@Value("${boot.scratch.name}")
+	private String name;
+
+	
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+		System.out.println(name);
+		return "Hello World!";
+	}
+	
+}
